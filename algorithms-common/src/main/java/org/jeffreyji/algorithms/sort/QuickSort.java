c2268183245c35ae0357e69b46cb5dfefdbfb405
jeffreyji <gefforey520@gmail.com>
2014-03-27 19:25:31 +0800

    add backtracking

--- algorithms-common/src/main/java/org/jeffreyji/algorithms/sort/QuickSort.java
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/sort/QuickSort.java
@@ -1,4 +1,4 @@
-package org.jeffreyji.algorithms;
+package org.jeffreyji.algorithms.sort;
 
 /**
  * @author: wgji
@@ -6,13 +6,13 @@ package org.jeffreyji.algorithms;
  * @comment:
  */
 public class QuickSort {
-    public static int[] x = { 3, 2, 4, 1, 6 };
 
     public static void main(String[] args) {
-        quicksort(0, x.length - 1);
+        int[] x = { 3, 2, 4, 1, 6 };
+        quickSort1(x, 0, x.length - 1);
     }
 
-    public static void quicksort(int l, int u) {
+    public static void quickSort1(int[] x, int l, int u) {
         int i, m;
         if (l >= u) {
             return;
@@ -20,34 +20,34 @@ public class QuickSort {
         m = l;
         for (i = l + 1; i <= u; i++) {
             if (x[i] < x[l]) { // buggy!
-                swap(++m, i);
+                swap(x, ++m, i);
             }
         }
-        printArray();
+        printArray(x);
         System.out.println("before swap xl:" + x[l] + ", xm:" + x[m]);
-        swap(l, m);
-        printArray();
+        swap(x, l, m);
+        printArray(x);
 
         System.out.println("----------------------");
-        quicksort(l, m - 1);
-        quicksort(m + 1, u);
+        quickSort1(x, l, m - 1);
+        quickSort1(x, m + 1, u);
 
     }
 
-    public static void swap(int i, int j) {
+    private static void swap(int[] x, int i, int j) {
         int tmp = x[i];
         x[i] = x[j];
         x[j] = tmp;
     }
 
-    public static void printArray() {
+    private static void printArray(int[] x) {
         for (int i = 0; i < x.length; i++) {
             System.out.print(x[i]);
         }
         System.out.println();
     }
 
-    public static int partition(int[] a, int left, int right) {
+    private static int partition(int[] a, int left, int right) {
         int temp;
 
         temp = a[left];
@@ -62,17 +62,19 @@ public class QuickSort {
         }
         a[left] = temp;
 
+        printArray(a);
+
         return left;
     }
 
-    public static void quickSort(int[] a, int left, int right) {
+    public static void quickSort2(int[] a, int left, int right) {
         int pivot;
 
         if (left >= right)
             return;
 
         pivot = partition(a, left, right);
-        quickSort(a, left, pivot - 1);
-        quickSort(a, pivot + 1, right);
+        quickSort2(a, left, pivot - 1);
+        quickSort2(a, pivot + 1, right);
     }
 }
