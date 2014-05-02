42b0717e8888554a8bdf138a9c7545bccaa06f83
jeffreyji <gefforey520@gmail.com>
2014-05-02 15:38:18 +0800

    add merge sort and leetcode sort linked list

---
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/MergeSortLinkedList.java
@@ -0,0 +1,63 @@
+package org.jeffreyji.algorithms.leetcode;
+
+/**
+ * @author: wgji
+ * @date：2014年5月2日 下午2:23:56
+ * @comment:
+ */
+public class MergeSortLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = findMiddle(head);
+
+        ListNode right = mergeSort(mid.next);
+        mid.next = null;
+        ListNode left = mergeSort(head);
+
+        return merge(left, right);
+    }
+
+    /**
+     * find the middle
+     * 
+     * @param head
+     * @return
+     */
+    private static ListNode findMiddle(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
+    }
+}
---
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/sort/MergeSort.java
@@ -0,0 +1,58 @@
+package org.jeffreyji.algorithms.sort;
+
+/**
+ * @author: wgji
+ * @date：2014年5月2日 下午12:23:16
+ * @comment:
+ */
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35,
+                25, 53, 51 };
+        mergeSort(a, 0, a.length - 1);
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+    }
+
+    public static void mergeSort(int[] data, int left, int right) {
+        if (left < right) {
+            // 找出中间索引
+            int center = (left + right) / 2;
+            // 对左边数组进行递归
+            mergeSort(data, left, center);
+            // 对右边数组进行递归
+            mergeSort(data, center + 1, right);
+            // 合并
+            merge(data, left, center, right);
+
+        }
+    }
+
+    public static void merge(int[] data, int left, int center, int right) {
+        int[] res = new int[data.length];
+        int mid = center + 1;
+        // third记录中间数组的索引
+        int i = left;
+        int tmp = left;
+        while (left <= center && mid <= right) {
+            // 从两个数组中取出最小的放入中间数组
+            if (data[left] <= data[mid]) {
+                res[i++] = data[left++];
+            } else {
+                res[i++] = data[mid++];
+            }
+        }
+        // 剩余部分依次放入中间数组
+        while (mid <= right) {
+            res[i++] = data[mid++];
+        }
+        while (left <= center) {
+            res[i++] = data[left++];
+        }
+        // 将中间数组中的内容复制回原数组
+        while (tmp <= right) {
+            data[tmp] = res[tmp++];
+        }
+    }
+}
