2226cf0cfc49967676189b670fe474ae3616c9d7
jeffreyji <gefforey520@gmail.com>
2014-05-03 22:29:33 +0800

    correct sliding window code

--- algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/ThreeSum.java
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/ThreeSum.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
  * @date：2014年5月3日 上午11:16:58
  * @comment:
  */
-
 public class ThreeSum {
     public static void main(String[] args) {
         int[] data = { -1, 0, 1, 2, -1, -4 };
@@ -21,34 +20,36 @@ public class ThreeSum {
         }
     }
 
-    public static ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
-        if (num == null || num.length < 3) {
-            return rst;
+    public static ArrayList<ArrayList<Integer>> threeSum(int[] data) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+
+        if (data == null || data.length < 3) {
+            return res;
         }
-        Arrays.sort(num);
-        for (int i = 0; i < num.length - 2; i++) {
-            if (i != 0 && num[i] == num[i - 1]) {
-                continue; // to skip duplicate numbers; e.g [0,0,0,0]
+        Arrays.sort(data);
+
+        for (int i = 0; i < data.length - 2; i++) {
+            if (i != 0 && data[i] == data[i - 1]) {
+                continue;
             }
 
             int left = i + 1;
-            int right = num.length - 1;
+            int right = data.length - 1;
             while (left < right) {
-                int sum = num[left] + num[right] + num[i];
+                int sum = data[left] + data[i] + data[right];
                 if (sum == 0) {
-                    ArrayList<Integer> tmp = new ArrayList<Integer>();
-                    tmp.add(num[i]);
-                    tmp.add(num[left]);
-                    tmp.add(num[right]);
-                    rst.add(tmp);
+                    ArrayList<Integer> item = new ArrayList<Integer>();
+                    item.add(data[left]);
+                    item.add(data[i]);
+                    item.add(data[right]);
+                    res.add(item);
                     left++;
                     right--;
-                    while (left < right && num[left] == num[left - 1]) { // to skip duplicates
+                    while (left < right && data[left] == data[left - 1]) {
                         left++;
                     }
-                    while (left < right && num[right] == num[right + 1]) { // to skip duplicates
-                        right--;
+                    while (left < right && data[right] == data[right + 1]) {
+                        right++;
                     }
                 } else if (sum < 0) {
                     left++;
@@ -57,6 +58,6 @@ public class ThreeSum {
                 }
             }
         }
-        return rst;
+        return res;
     }
 }
--- algorithms-common/src/main/java/org/jeffreyji/algorithms/util/SlideWindow1.java
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/util/SlideWindow1.java
@@ -1,29 +1,33 @@
 package org.jeffreyji.algorithms.util;
 
-import java.util.Arrays;
-
 /**
  * @author: wgji
  * @date：2014年5月1日 下午2:38:02
- * @comment:
+ * @comment:最近，afy决定给TOJ印刷广告，广告牌是刷在城市的建筑物上的，城市里有紧靠着的N个建筑。
+ * afy决定在上面找一块尽可能大的矩形放置广告牌。我们假设每个建筑物都有一个高度，
+ * 从左到右给出每个建筑物的高度H1,H2…HN，且0<Hi<=1,000,000,000，并且我们假设每个建筑物的宽度均为1。
+ * 要求输出广告牌的最大面积。
+ * 【输入样例】
+ * 6
+ * 5 8 4 4 8 4
+ * 【输出样例】
+ * 24
  */
-
 public class SlideWindow1 {
     public static void main(String[] args) {
-        int[] x = {5, 7, 7, 8, 8, 10};
-        System.out.println(Arrays.binarySearch(x, 8));
-        
-//        int[] h = { 5, 8, 4, 4, 8, 4 };
-//        int[] mq = new int[h.length]; // 单调队列，对内元素为建筑物高度的下标
-//        int[] left = new int[h.length]; // left[i]：在第i个建筑物左侧，不比它的高度小的建筑物数量
-//        int[] right = new int[h.length]; // right[i]：在第i个建筑物右侧，不比它的高度小的建筑物数量
-//
-//        calcLeft(h, mq, left);
-//        calcRight(h, mq, right);
-//        System.out.println(maxRectArea(h, left, right));
+        int[] h = { -1, 5, 8, 4, 4, 8, 4, -1 };
+        int[] mq = new int[h.length + 1]; // 单调队列，对内元素为建筑物高度的下标
+        int[] left = new int[h.length + 1]; // left[i]：在第i个建筑物左侧，不比它的高度小的建筑物数量
+        int[] right = new int[h.length + 1]; // right[i]：在第i个建筑物右侧，不比它的高度小的建筑物数量
+
+        calcLeft(h, mq, left);
+        calcRight(h, mq, right);
+        System.out.println(maxRectArea(h, left, right));
+        System.out.println(maxRectArea2(h));
+
     }
 
-    public static int MaxRectArea2(int[] h) {
+    public static int maxRectArea2(int[] h) {
         int maxArea = 0;
         int i, j;
         for (i = 0; i < h.length; i++) {
@@ -47,7 +51,7 @@ public class SlideWindow1 {
     public static void calcLeft(int[] h, int[] mq, int[] left) {
         mq[0] = 0;
         int front = 0, rear = 1;
-        for (int i = 1; i < h.length; i++) {
+        for (int i = 1; i <= h.length - 2; i++) {
             while (front < rear && h[i] <= h[mq[rear - 1]]) {
                 rear--;
             }
@@ -57,11 +61,11 @@ public class SlideWindow1 {
     }
 
     public static void calcRight(int[] h, int[] mq, int[] right) {
-        mq[0] = h.length + 1;
+        mq[0] = h.length - 1;
         int front = 0, rear = 1;
 
         int i;
-        for (i = h.length - 1; i >= 1; i--) {
+        for (i = h.length - 2; i >= 1; i--) {
             while (front < rear && h[i] <= h[mq[rear - 1]]) {
                 rear--;
             }
@@ -72,8 +76,7 @@ public class SlideWindow1 {
 
     public static int maxRectArea(int[] h, int[] left, int[] right) {
         int maxArea = -1;
-        int i;
-        for (i = 1; i < h.length; i++) {
+        for (int i = 1; i < h.length - 1; i++) {
             int area = (left[i] + right[i] + 1) * h[i];
             if (area > maxArea) {
                 maxArea = area;
