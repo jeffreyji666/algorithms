5ad1c61212dec50897706449a71ed611f0106f6b
jeffreyji <gefforey520@gmail.com>
2014-05-21 17:47:37 +0800

    refactor code

--- algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/LongestPalindrome.java
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/LongestPalindrome.java
@@ -10,8 +10,8 @@ package org.jeffreyji.algorithms.leetcode;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-        System.out.println(longestPalindrome3("aadbcdddcbd"));
-        System.out.println(getPalindromeLength("aadbcdddcbd"));
+        //System.out.println(longestPalindrome3("aadbcdddcbd"));
+        System.out.println(getPalindromeLength("aadbcddcbd"));
     }
 
     public static String longestPalindrome(String s) {
@@ -156,20 +156,20 @@ public class LongestPalindrome {
         // 为了避免奇数回文和偶数回文的不同处理问题，在原字符串中插入'#'，将所有回文变成奇数回文
         StringBuilder newStr = new StringBuilder();
         newStr.append('#');
-        for (int i = 0; i < str.length(); i ++) {
+        for (int i = 0; i < str.length(); i++) {
             newStr.append(str.charAt(i));
             newStr.append('#');
         }
-        
+
         // rad[i]表示以i为中心的回文的最大半径，i至少为1，即该字符本身
-        int [] rad = new int[newStr.length()];
+        int[] rad = new int[newStr.length()];
         // right表示已知的回文中，最右的边界的坐标
         int right = -1;
         // id表示已知的回文中，拥有最右边界的回文的中点坐标
         int id = -1;
         // 2.计算所有的rad
         // 这个算法是O(n)的，因为right只会随着里层while的迭代而增长，不会减少。
-        for (int i = 0; i < newStr.length(); i ++) {
+        for (int i = 0; i < newStr.length(); i++) {
             // 2.1.确定一个最小的半径
             int r = 1;
             if (i <= right) {
@@ -180,20 +180,23 @@ public class LongestPalindrome {
                 r++;
             }
             // 2.3.更新边界和回文中心坐标
-            if (i + r - 1> right) {
+            if (i + r - 1 > right) {
                 right = i + r - 1;
                 id = i;
             }
             rad[i] = r;
         }
-        
+
         // 3.扫描一遍rad数组，找出最大的半径
         int maxLength = 0;
-        for (int r : rad) {
-            if (r > maxLength) {
-                maxLength = r;
+        int centerIndex = 0;
+        for (int i = 0; i < rad.length; i++) {
+            if (rad[i] > maxLength) {
+                maxLength = rad[i];
+                centerIndex = i;
             }
         }
+        System.out.println(str.substring((centerIndex - maxLength) / 2 + 1, maxLength + 1));
         return maxLength - 1;
     }
 }
