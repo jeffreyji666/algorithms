8e76deda2139ec2a5ecd1366485508287396a03b
jeffreyji <gefforey520@gmail.com>
2014-05-22 16:04:25 +0800

    add the solution of binary tree lru cache in leetcode

---
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/LRUCache.java
@@ -0,0 +1,43 @@
+package org.jeffreyji.algorithms.leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author: wgji
+ * @date：2014年5月22日 下午3:00:41
+ * @comment:
+ */
+
+public class LRUCache {
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(1);
+        cache.set(2, 1);
+        System.out.println(cache.get(2));
+        cache.set(3, 2);
+        System.out.println(cache.get(2));
+        System.out.println(cache.get(3));
+    }
+
+    private Map<Integer, Integer> cache = null;
+
+    public LRUCache(final int capacity) {
+        assert (capacity > 0);
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        Integer val = cache.get(key);
+        return val == null ? -1 : val;
+    }
+
+    public void set(int key, int value) {
+        cache.put(key, value);
+    }
+}
