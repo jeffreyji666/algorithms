b6e47a010ef0248720a7c769645b99b1131ac723
jeffreyji <gefforey520@gmail.com>
2014-05-24 00:05:33 +0800

    add the solution of remove duplicates from sort array 2 in leetcode

---
+++ algorithms-common/src/main/java/org/jeffreyji/algorithms/leetcode/RemoveDuplicatesFromSortArrays2.java
@@ -0,0 +1,39 @@
+package org.jeffreyji.algorithms.leetcode; 
+
+import java.util.Arrays;
+
+/** 
+ * @author:  wgji
+ * @date：2014年5月23日 下午11:44:48 
+ * @comment: Follow up for "Remove Duplicates":
+ * What if duplicates are allowed at most twice?
+ * For example,
+ * Given sorted array A = [1,1,1,2,2,3],
+ * Your function should return length = 5, and A is now [1,1,2,2,3].
+ */
+
+public class RemoveDuplicatesFromSortArrays2 {
+
+    public static void main(String[] args) {
+        int[] a = {1,1,1,2,2,3,3,3};
+        int length = removeDuplicates(a,a.length);
+        for(int i= 0 ; i < length; i++){
+            System.out.printf("%d,",a[i]);
+        }
+    }
+
+    public static int removeDuplicates(int a[], int n) {
+        if (n <= 2) {
+            return n; // no need to deal with n<=2 case
+        }
+        int len = 2, itor = 2;
+        while (itor < n) {
+            if (a[itor] != a[len - 2]) {
+                a[len++] = a[itor];
+            }
+            itor++;
+        }
+        return len;
+    }
+}
+ 
